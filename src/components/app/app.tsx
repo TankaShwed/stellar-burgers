a9bfebe49d33e432792f3c4d33239aeaba052d26
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-07-12 10:01:05 +0300

    modal components

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,6 @@
 import {
   ConstructorPage,
   ForgotPassword,
-  Login,
   NotFound404,
   Profile,
   ProfileOrders,
@@ -12,57 +11,46 @@ import { Feed } from '../../pages/feed';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
-// import { ProtectedRoute } from '../protected-route/protected-route';
+import { AppHeader, IngredientDetails, Modal } from '@components';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import { loadIngredientsNogaThunk } from '../../features/ingredient-slice/ingredient-slice';
 import { useDispatch } from '../../services/store';
 
 const App = () => {
-  console.log('app');
   const dispatch_noga = useDispatch();
   useEffect(() => {
     dispatch_noga(loadIngredientsNogaThunk());
   }, []);
+  const location = useLocation();
+  const backgroundLocation = location.state?.background;
   return (
     <div className={styles.app}>
       <AppHeader />
-      <BrowserRouter>
+      <Routes location={backgroundLocation || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profile/orders' element={<ProfileOrders />} />
+        <Route path='/ingredients/:id' element={<IngredientDetails />} />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      {backgroundLocation && (
         <Routes>
-          <Route path='/' element={<ConstructorPage />} />
-          <Route path='/feed' element={<Feed />} />
-          // Защищённые маршруты
-          {/* <Route path='/login' element={
-        <ProtectedRoute accessRoles={[ROLE.ADMIN]}>
-        <Login />
-      </ProtectedRoute>
-        
-        } /> */}
-          <Route path='/register' element={<Register />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/reset-password' element={<ResetPassword />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/profile/orders' element={<ProfileOrders />} />
-          // Маршрут по умолчанию
-          <Route path='/ingredients/:id' element={<IngredientDetails />} />
-          <Route path='*' element={<NotFound404 />} />
-          //модалки с дополнительной информацией
-          {/* <Route
-          path='/feed/:number'
-          сomponent={(props) => (
-            <>
-              {' '}
-              {props.match.params.number ? (
-                <Modal component={OrderInfo} />
-              ) : null}
-              <Feed />
-            </>
-          )}
-        /> */}
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal onClose={() => 0} title={''}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
         </Routes>
-      </BrowserRouter>
+      )}
     </div>
   );
 };
