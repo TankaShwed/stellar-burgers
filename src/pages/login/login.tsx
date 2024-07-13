d3c8686445bdd5f5db1812d124a472999dbd509e
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-07-13 10:05:07 +0300

    navigate for login component

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,24 +1,34 @@
-import { FC, SyntheticEvent, useState } from 'react';
+import { FC, SyntheticEvent, useEffect, useState } from 'react';
 import { LoginUI } from '@ui-pages';
 import { useDispatch, useSelector } from '../../services/store';
 import { loginThunk } from '../../features/user-slice/user-slice';
+import { useNavigate } from 'react-router-dom';
 
 export const Login: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const error = useSelector(st=>st.user.error);
+  const user = useSelector((st) => st.user.user);
+  const navigate = useNavigate();
+  const error = useSelector((st) => st.user.error);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user]);
   const dispatch = useDispatch();
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
-    dispatch(loginThunk({
-      email: email,
-      password: password
-    }))
+    dispatch(
+      loginThunk({
+        email: email,
+        password: password
+      })
+    );
   };
-  console.log('d1', error)
   return (
     <LoginUI
-      errorText={error|| ''}
+      errorText={error || ''}
       email={email}
       setEmail={setEmail}
       password={password}
