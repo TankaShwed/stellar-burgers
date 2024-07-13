796b9ecaf97a9b74994e7e198d35a239dd235116
Tanya Shvedyuk <shwedyuk.tatyana@yandex.ru>
2024-07-13 19:18:05 +0300

    navlinks

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -7,32 +7,35 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+const classNameFN = ({ isActive }: { isActive: boolean }) =>
+  isActive ? `${styles.link} ${styles.link_active}` : styles.link;
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <Link to="/">
+        <NavLink to='/' className={classNameFN}>
           <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </Link>
-        <Link to="/feed">
+        </NavLink>
+        <NavLink to='/feed' className={classNameFN}>
           <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </Link>
+        </NavLink>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
       </div>
-      <Link to='/profile' className={styles.link_position_last}>
+      <NavLink to='/profile' className={classNameFN}>
         <>
           <ProfileIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>
             {userName || 'Личный кабинет'}
           </p>
         </>
-      </Link>
+      </NavLink>
     </nav>
   </header>
 );
